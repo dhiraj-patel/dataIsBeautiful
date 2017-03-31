b82cac2846a0be52d6c00ab60de0c4a2e9c065c4
alasday <alasday@stuy.edu>
2017-03-31 14:08:28 -0400

    adding csv stuff in py

--- app.py
+++ app.py
@@ -2,6 +2,26 @@ from flask import Flask
 app = Flask(__name__)
 
 
+import csv
+
+f= open('data/strikes_by_president.csv')
+csv_f = csv.reader(f)
+
+for row in csv_f:
+    print row
+
+print
+print
+print
+
+g= open('data/strike_data.csv')
+csv_g = csv.reader(g)
+
+for row in csv_g:
+        print row
+
+
+    
 @app.route('/')
 def hello():
     return "Hello World!"
