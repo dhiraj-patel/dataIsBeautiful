00d33eae23cc9c5841166f24311536957200b858
alasday <alasday@stuy.edu>
2017-04-02 23:41:26 -0400

    fixing the fxn

--- app.py
+++ app.py
@@ -2,30 +2,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
-data = ""
 
-import csv
-f= open('data/strikes_by_president.csv')
-csv_f = csv.reader(f)
 
-k = 0
 
-for row in csv_f:
-	
-	i = 0
-        if k !=0:
-		for elem in row:
+
+
+
+
+def presidents():
+	import csv
+	f= open('data/strikes_by_president.csv')
+	csv_f = csv.reader(f)
+	data = ""
+	k = 0
+	for row in csv_f:
+		i = 0
+		if k !=0:
+			for elem in row:
         
-			if i == 0:
-				data = data + elem + ", "
-			if i == 1:
-				data = data + elem + "/n "
-			i+=1
-	k+=1
-
-print data
-
-return data
+				if i == 0:
+					data = data + elem + ", "
+			
+				if i == 1:
+					data = data + elem + "/n "
+				i+=1
+		k+=1
+	print data
+	return data
 
 
 '''
@@ -43,3 +46,5 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
+
+presidents()
